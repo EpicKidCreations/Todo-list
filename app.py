b94f2fc56c11cea890fff3d6fd9413e6d7b2a5f2
Pranav Kethireddy <63430515+EpicKidCreations@users.noreply.github.com>
2021-02-28 08:57:39 -0800

    last day code

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import pandas as pd
 import os
 import os.path
 from os import path
-
+x=datetime.datetime.now()
+currentyear = x.year
 
 app = Flask(__name__)
 
@@ -17,8 +18,6 @@ def home():
 
 @app.route('/todo', methods = ['GET', 'POST'])
 def todo():
-    x=datetime.datetime.now()
-    currentyear = x.year
     if request.method == 'GET':
         return render_template('form.html', currentyear=currentyear) 
 
@@ -26,14 +25,13 @@ def todo():
         email = request.form['email']
         task = request.form['task']
         hour = request.form['hour']
-        minute = request.form['minute']              
-        year = request.form['year']              
+        minute = request.form['minute']                          
         month = request.form['month']
         day = request.form['day']
         ts = x.timestamp()
-        task_list = [ts, email, task, hour, minute, year, month, day]
+        task_list = [ts, email, task, hour, minute, month, day]
         savecsv(task_list, "entry.csv")
-        return render_template('form.html', email=email, task=task, hour=hour, minute=minute, year=year, month=month, day=day, currentyear=currentyear)#, readcsv1=readcsv1)
+        return render_template('form.html', email=email, task=task, hour=hour, minute=minute, month=month, day=day, currentyear=currentyear)#, readcsv1=readcsv1)
 
 @app.route('/retrive', methods = ['Get', 'POST'])
 def retrive():
@@ -42,36 +40,39 @@ def retrive():
     
     if request.method == 'POST':
         email = request.form['email']
-        df = pd.read_csv("entry.csv")
-        sorted_df = df.sort_values(["month", "day", "year", "hour", "minute"], ascending=True)
-        sorted_df.to_csv('entry_sorted.csv', index=False)
-        with open('entry_sorted.csv', mode='r') as entry_file:
+        with open('entry.csv', mode='r') as entry_file:
             f = open('form.html', 'w')
             csv_reader = csv.DictReader(entry_file)
             raw = []
             for row in csv_reader:
                 if row["email"] == email:
-                    #raw.append(f'{row["email"]} has {row["task"]} due on {row["month"]} {row["day"]}, {row["year"]} at {row["hour"]}:{row["minute"]}')
-                    #ids = row["id"]
                     raw.append(row)
         return render_template('form.html', raw=raw)
 
+@app.route('/delete', methods =['GET', 'POST'])
+def delete1():
+    checkbox = request.form['checkbox']
+    delete(checkbox)
+    return render_template('form.html')
+
 def delete(delete):
-    #1. This code snippet asks the user for a username and deletes the user's record from file.
-    updatedlist=[]
-    with open("entry_sorted.csv",newline="") as f:
-      reader=csv.reader(f)
-      delete_item=delete
-      
-      for row in reader: #for every row in the file
-            
-                if row[0]!=delete_item:
-                    updatedlist.append(row) #add each row, line by line, into a list called 'udpatedlist'
-      return(updatedlist)
-      updatefile(updatedlist)
+    lines = list()
+    with open('entry.csv', 'r') as readFile:
+        reader = csv.reader(readFile)
+        for row in reader:
+            lines.append(row)
+            for field in row:
+                if field == delete:
+                    lines.remove(row)
+    with open('entry.csv', 'w') as writeFile:
+        writer = csv.writer(writeFile)
+        writer.writerows(lines)
 
 def savecsv(task_list, csv_file):
     with open(csv_file, mode='a') as entry_file:
+        df = pd.read_csv("entry.csv")
+        sorted_df = df.sort_values(["month", "day", "hour", "minute"], ascending=True)
+        sorted_df.to_csv('entry.csv', index=False)
         writer_object = writer(entry_file)
         writer_object.writerow(task_list)
         entry_file.close()
